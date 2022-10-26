cd399e41cadb71fa7c25e6206bcd5502a2fe8b0c
gohiy <57204793+rinze70@users.noreply.github.com>
2022-10-26 16:29:29 +0000

    Del redundant Feature matching/classification.

--- classification/configs/pvt/pvt_medium.py
+++
@@ -1,6 +0,0 @@
-cfg = dict(
-    model='pvt_medium',
-    drop_path=0.3,
-    clip_grad=1.0,
-    output_dir='checkpoints/pvt_medium',
-)
--- classification/configs/quadtree/b4.py
+++
@@ -1,6 +0,0 @@
-cfg = dict(
-    model='quadtree_b4',
-    drop_path=0.3,
-    clip_grad=1.0,
-    output_dir='checkpoints/quadtree_b4',
-)
