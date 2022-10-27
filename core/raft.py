75d3dc507dc0ebf38690b3d6e4579247ea4751dd
gohiy <57204793+rinze70@users.noreply.github.com>
2022-10-27 18:42:13 +0000

    Disable quadtree.

--- core/raft.py
+++ core/raft.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 
 from update import BasicUpdateBlock, SmallUpdateBlock
 from extractor import BasicEncoder, SmallEncoder
-from corr import CorrBlock, AlternateCorrBlock, QuadTreeCorrBlock
+from corr import CorrBlock, AlternateCorrBlock
 from utils.utils import bilinear_sampler, coords_grid, upflow8
 
 from encoders import twins_svt_large
@@ -125,8 +125,8 @@ class RAFT(nn.Module):
         # fmap2 = fmap2.float()
         if self.args.alternate_corr:
             corr_fn = AlternateCorrBlock(fmap1, fmap2, radius=self.args.corr_radius)
-        elif self.args.quad_tree:
-            corr_fn = QuadTreeCorrBlock(fmap1, fmap2, topks=[16, 8, 8], radius=self.args.corr_radius)
+        # elif self.args.quad_tree:
+        #     corr_fn = QuadTreeCorrBlock(fmap1, fmap2, topks=[16, 8, 8], radius=self.args.corr_radius)
         else:
             corr_fn = CorrBlock(fmap1, fmap2, radius=self.args.corr_radius)
 
